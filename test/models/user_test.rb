35b6daa2360705b5f5d4e3b4be5ef1e9ca856fed
DavidLineman <dline215@gmail.com>
2019-07-19 13:41:20 -0400

    added user model adn tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,26 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+
+  test "should be valid" do
+    assert @user.valid?
+  end
+
+  test "name should be present" do 
+    @user.name = "   "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.email = "a" * 51 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+    @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
 end
